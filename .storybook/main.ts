3217e5a6e37eeff50ae708b16b3e14aa466d3244
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2024-02-11 14:53:34 -0500

    Fix storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,6 @@ const config: StorybookConfig = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
-    '@storybook/addon-styling-webpack'
   ],
   framework: {
     name: '@storybook/vue3-vite',
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { default as Button } from './components/m-button/Button.vue'
+export { default as Button } from './components/m-button/MButton.vue'
