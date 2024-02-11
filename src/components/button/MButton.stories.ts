4b766bb167d52e7717886b5183870e2d004e0474
Tucker Hawkinson <hawkinson.tucker@gmail.com>
2024-02-11 14:22:30 -0500

    Set up storybook

---
+++ src/components/button/MButton.stories.ts
@@ -0,0 +1,21 @@
+import type { Meta, StoryObj } from '@storybook/vue3'
+
+import MButton from './MButton.vue'
+
+const meta = {
+  title: 'Base/MButton',
+  component: MButton,
+  tags: ['autodocs'],
+  argTypes: {
+    text: { control: 'text' },
+  },
+} satisfies Meta<typeof MButton>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Text: Story = {
+  args: {
+    text: 'Test',
+  },
+}
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,1 @@
-export { default as Button } from './components/Button.vue'
+export { default as Button } from './components/button/Button.vue'
